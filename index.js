609f54244903726bfdcd2b9e72b002929d316cac
abrajelakotobia <103642520+abrajelakotobia@users.noreply.github.com>
2023-06-22 15:02:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,31 +8,28 @@ import roomsRoute from "./routes/rooms.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-const app = express();
-dotenv.config();
+const app = express()
+dotenv.config()
 
-const connect = async () => {
-  try {
+const connect = async () =>{
+try {
     await mongoose.connect(process.env.MONGO);
-    console.log("Connected to mongoDB.");
+    console.log("Conected to mongoDB.")
   } catch (error) {
     throw error;
   }
 };
-
-mongoose.connection.on("disconnected", () => {
-  console.log("mongoDB disconnected!");
-});
-
-//middlewares
-app.use(cors())
-app.use(cookieParser())
+mongoose.connection.on("disconnected", ()=>{
+   console.log("mongoDB disconnected")
+})
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", usersRoute);
-app.use("/api/hotels", hotelsRoute);
-app.use("/api/rooms", roomsRoute);
+app.use("/api/auth",authRoute);
+app.use("/api/users",usersRoute);
+app.use("/api/hotels",hotelsRoute);
+app.use("/api/rooms",roomsRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
@@ -44,9 +41,9 @@ app.use((err, req, res, next) => {
     stack: err.stack,
   });
 });
-
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 connect()
-app.listen(port, () => {
-  console.log(`Server run on port ${port}`);
-});
+app. listen(PORT,
+    console.log(`server run in port ${PORT}`
+));
+
